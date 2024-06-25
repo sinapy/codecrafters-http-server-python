b36de1cec04b847800f1ebde9c5ede4504615234
sinapy <sinasarsharpour@cmail.carleton.ca>
2024-06-24 21:36:39 -0400

    test

--- app/main.py
+++ app/main.py
@@ -6,7 +6,11 @@ def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, address = server_socket.accept() # wait for client
-    connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    data = connection.recv(1024).split()[1]
+    if (data == b"/"):
+        connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 if __name__ == "__main__":
     main()
