0da8288400cc1539887516e1a34aba604c0079db
sinapy <sinasarsharpour@cmail.carleton.ca>
2024-06-24 21:21:29 -0400

    test

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ import socket
 def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n") # wait for client
-
+    connection, address = server_socket.accept() # wait for client
+    connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
     main()
