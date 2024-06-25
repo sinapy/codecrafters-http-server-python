7f3bf53f5cd514ec9e3f1020b9a73143a189d0be
sinapy <sinasarsharpour@cmail.carleton.ca>
2024-06-24 23:50:19 -0400

    FS3

--- app/main.py
+++ app/main.py
@@ -13,6 +13,10 @@ def main():
         connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
     elif route.split(b"/")[1] == b"echo" and len(route.split(b"/")) == 3 and route.split(b"/")[2] != b"":
         connection.sendall(b"HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-Length: %d\r\n\r\n%s" % (len(route.split(b"/")[2]), route.split(b"/")[2]))
+    elif route.split(b"/")[1] == b"user-agent":
+        print(data.split(b"\r\n"))
+        user_agent = [x for x in data.split(b"\r\n") if x.startswith(b"User-Agent:")][0].removeprefix(b"User-Agent:").strip()
+        connection.sendall(b"HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-Length: %d\r\n\r\n%s" % (len(user_agent), user_agent))
     else:
         connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
